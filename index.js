e444e8930a0f8a77b51a391d5bdf0799ed2805e7
Aleksandra Kosc <aleksandra.kosc@gmail.com>
2019-06-02 18:40:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var name = "Joe"
 var height = "74"
 if (name === "Susan") { console.log("The name is correct") }
-
+else (name === "Susan") { console.log("Expected nema to be Susan") }
 // Don't worry about this
 module.exports = { name, height
 }
